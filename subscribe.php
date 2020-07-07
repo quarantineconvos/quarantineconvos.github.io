520032a4e11ea49dc8f3592cb1cbc2dcd6851a61
quarantineconvos <67770110+quarantineconvos@users.noreply.github.com>
2020-07-06 23:41:39 -0400

    Basic php mail stuff
    wont work tho!

--- subscribe.php
+++ subscribe.php
@@ -1,1 +1,11 @@
+<?php
+$name = $_POST['name'];
+$from = "conversationsq@gmail.com";
+$to = $_POST['mail'];
+//var_dump($_POST['mail']);
+$subject = "Thank you!";
+$message= "Thank you for joining our mailing list {$name}! We hope to live up to your expectations";
+$headers = "From:" . $from;
+mail ($to,$subject,$message,$headers);
 
+?>
