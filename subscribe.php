577a7e01382bc9444a76489f88c32745bb4efcd0
digiArchitect <66040569+digiArchitect@users.noreply.github.com>
2020-07-07 01:20:36 -0400

    Added vendor so it *hopefully* works on the site
    In addition I also added to the subscribe.php stuff

--- subscribe.php
+++ subscribe.php
@@ -2,16 +2,23 @@
 
 use PHPMailer\PHPMailer\PHPMailer;
 
-require 'PHPMailerAutoload.php';
+//require 'PHPMailerAutoload.php';
+require 'path/to/vendor/autoload.php';
 $mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'conversationsq@gmail.com';
+$mail->Password = 'threelevelyacht';
+$mail->SMTPSecure = "tls";   
+$mail->Port = 587;   
 $name = $_POST['name'];
 $from = "conversationsq@gmail.com";
 
 $to = $_POST['mail'];
 //var_dump($_POST['mail']);
-$subject = "Thank you!";
-$message= "Thank you for joining our mailing list {$name}! We hope to live up to your expectations";
-$headers = "From:" . $from;
-mail ($to,$subject,$message,$headers);
-
+$mail->setFrom($mail, 'The Quarantine Conversations Staff');
+$mail->addAddress($to, $name);
+$mail->Subject = "Thank you!";
+$mail->Body ="Thank you for subscribing to the Quarantine Conversations newsletter. We hope we exceed the expectations you have for"
 ?>
